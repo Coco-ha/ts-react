d9f622c04346f9fe76fd3ece18a24d701aadfd09
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-09-04 19:11:11 -0500

    inital

---
+++ boxes/src/BoxList.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+import { BoxInterface, BoxListInterface } from "./interfaces";
+
+/** Manage list of boxes
+ *
+ * State:
+ * - boxes: [ { id, width, height, backgroundColor }, ... ]
+ */
+
+function BoxList() {
+  const [boxes, setBoxes] = useState<BoxListInterface[]>([])
+
+  /** add box with given { id, width, height, backgroundColor } */
+  function add(newBox) {
+    setBoxes(boxes => [...boxes, newBox]);
+  }
+
+  /** remove box matching that id. */
+  function remove(id: string) {
+    setBoxes(boxes => boxes.filter(box => box.id !== id));
+  }
+
+  return (
+    <div className="BoxList">
+      <NewBoxForm createBox={add} />
+      {boxes.map(({ id, width, height, backgroundColor }: BoxListInterface) => (
+        <Box
+          key={id}
+          id={id}
+          width={width}
+          height={height}
+          remove={remove}
+          backgroundColor={backgroundColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default BoxList;
---
+++ boxes/src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface BoxInterface{
+  id: string,
+  height: number,
+  width: number,
+  backgroundColor: string,
+  remove(id:any)
+}
+export interface BoxListInterface{
+  id: string,
+  height: number,
+  width: number,
+  backgroundColor: string,
+}
